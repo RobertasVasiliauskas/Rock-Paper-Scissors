0290fd0740d519e19cdb973c8f5d4650acc6b1c1
Your Name <you@example.com>
2023-10-31 14:13:55 +0200

    added final logic
    Game is playable and has basic logic
    
    Signed-off-by: Your Name <you@example.com>

--- gameLogic.js
+++ gameLogic.js
@@ -21,4 +21,21 @@ function getComputerChoice() {
     return choice;
 }
 
-console.log(getComputerChoice());
+function playRound(computerSelection, playerSelection) {
+    let result;
+
+    if (computerSelection === playerSelection) result = "Draw";
+    else if (computerSelection === "rock" && playerSelection === "scissors") result = "Computer wins";
+    else if (computerSelection === "scissors" && playerSelection === "paper") result = "Computer wins";
+    else if (computerSelection === "paper" && playerSelection === "rock") result = "Computer wins";
+    else result = "Player wins";
+
+    return result;
+}
+
+let playerChoice = "Rock";
+let computerChoice = getComputerChoice();
+console.log(computerChoice, playerChoice, playRound(computerChoice.toLowerCase(), playerChoice.toLowerCase()));
+
+
+

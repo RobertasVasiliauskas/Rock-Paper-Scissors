73193c2cfb22e9756cc38c87d5267d77abc1e70e
Your Name <you@example.com>
2023-10-31 14:24:45 +0200

    Final logic update, logic is completed
    At this stage of project logic is completed,
    added player input from keyboard, final winner count.
    
    Signed-off-by: Your Name <you@example.com>

--- gameLogic.js
+++ gameLogic.js
@@ -33,9 +33,29 @@ function playRound(computerSelection, playerSelection) {
     return result;
 }
 
-let playerChoice = "Rock";
-let computerChoice = getComputerChoice();
-console.log(computerChoice, playerChoice, playRound(computerChoice.toLowerCase(), playerChoice.toLowerCase()));
+function game(){
+    let count = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let winner;
+        let playerChoice = prompt();
+        let computerChoice = getComputerChoice();
+
+        winner = playRound(computerChoice.toLowerCase(), playerChoice.toLowerCase());
+        console.log(computerChoice, playerChoice, winner);
+
+        if(winner === "Computer wins") count += 1;
+    }
+
+    if (count >= 3) return "Computer won most rounds";
+    else if(count <= 3) return "Player won most rounds";
+    else return "Draw";
+
+}
+
+console.log(game());
+
+
 
 
 
